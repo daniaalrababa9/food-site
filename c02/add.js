50baa2470526e65c6282292454450edcbfcc8e66
daniaalrababa9 <rababadania@gmail.com>
2019-09-25 14:49:45 +0300

    add function

--- c02/add.js
+++ c02/add.js
@@ -1,28 +1,51 @@
+
+function createGreeting() {
+    // input
+    var hourNow ;
+    var greeting;
+  
+    // processing:
+    if (hourNow > 18) {
+      greeting = 'Good evening, Class!';
+    } else if (hourNow >= 12) {
+      greeting = 'Good afternoon, Class!';
+    } else if (hourNow >= 0) {
+      greeting = 'Good morning!';
+    } else {
+      greeting = 'Welcome!';
+    }
+  
+    //output:
+    return greeting;
+  }
 var dania ,age
-dania = 'my name is dania'
-age = 'my age is'
+dania = 'My name is dania'
+age = 'My age is'
 var x, y, z;
 x = 10;
 y = 16;
 z = x + y;
+var owner
+owner = 'Iam the owner of this website'
 document.write('<h3>'+ dania +'</h3>');
 document.write('<h3>'+ age +'</h3>');
 document.write('<h3>'+ z +'</h3>');
+document.write('<h3>'+ owner +'</h3>');
 
 
-var yourweight = prompt ('your weight');
-var greeting ;
+var yourweight = prompt (' Inter your weight in Kg, please');
+var word ;
 if (yourweight > 100) {
-    greeting = 'fat';
+    word = 'over weight';
 } else if (yourweight > 50 && yourweight < 100) {
-    greeting = 'average';
+    word = 'average';
 } else if (yourweight < 50) {
-    greeting = 'skinny';   
+    word = 'skinny';   
 } else {
-    greeting = 'welcome!';
+    word = 'error';
 }
 
-document.write('<h3>'+ greeting +'</h3>');
+
 
 
 
